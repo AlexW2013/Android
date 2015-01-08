6b15df38a35ed76f4bd27131730e1032e0e8a2c6
Alex Wang <wang4112@umn.edu>
2015-01-08 01:39:15 -0600

    Adding Retirement Fund app

---
+++ RetirementFundPlanner/README.md
@@ -0,0 +1,5 @@
+A simple android app that functions as a retirement funds calculator
+- Input funds for 401k, Stocks/funds, and other investments
+- Calculates final value for each, and a combined total value
+
+The app will be improved and worked on little by little. Possible future additions of past financial history, such as historic sp500 returns, and graphs that show overall fund growth.
---
+++ RetirementFundPlanner/app/src/main/java/com/alexw/retirementfundplanner/OptionOther.java
@@ -0,0 +1,85 @@
+package com.alexw.retirementfundplanner;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class OptionOther extends ActionBarActivity {
+
+    protected static int otherBalance, otherAddition, otherInterest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option_other);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_option_other, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void returnScreen(View view){
+
+        //Retrieve values from editText Fields
+        EditText balOtherText = (EditText) findViewById(R.id.bal_other);
+        EditText addOtherText = (EditText) findViewById(R.id.add_other);
+        EditText growthOtherText = (EditText) findViewById(R.id.int_other);
+
+        // Exception checks - if any text field is blank, a popup will appear
+        // Possibly find a way to make this it's own method without crashing app
+
+        String balOther = balOtherText.getText().toString();
+        if(TextUtils.isEmpty(balOther)){
+            Toast.makeText(this, "Please enter a value for current 401k Balance!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        String addOther = addOtherText.getText().toString();
+        if(TextUtils.isEmpty(addOther)){
+            Toast.makeText(this, "Please enter a value for current Income!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String growthOther = growthOtherText.getText().toString();
+        if(TextUtils.isEmpty(growthOther)){
+            Toast.makeText(this, "Please enter a value for expected Raise", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        //Set final values for stock page
+        otherBalance = Integer.parseInt(balOther);
+        otherAddition = Integer.parseInt(addOther);
+        otherInterest = Integer.parseInt(growthOther);
+
+        //Return to main page
+        finish();
+    }
+
+}
